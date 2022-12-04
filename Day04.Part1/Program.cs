d4aa2789b2f07979dd611e1a24fb0f27cf687c4e
Phomor <42185162+Phomor@users.noreply.github.com>
2022-12-04 14:51:59 +0100

    Day 04 Part 1

---
+++ Day04.Part1/Program.cs
@@ -0,0 +1,41 @@
+﻿var score = 0;
+
+try
+{
+    using var sr = new StreamReader("F:\\Documents\\AdventOfCode\\Day04.Part1\\input");
+    string? line;
+    while ((line = sr.ReadLine()) != null)
+    {
+        var ass1 = new Assignment(line[..line.IndexOf(",")]);
+        var ass2 = new Assignment(line[(line.IndexOf(",") + 1)..]);
+        if (ass1.IsContained(ass2))
+        {
+            score++;
+        }
+    }
+
+    Console.WriteLine(score);
+}
+catch (IOException ex)
+{
+    Console.WriteLine("File could not be read");
+    Console.WriteLine(ex.ToString());
+}
+
+class Assignment
+{
+    private int from;
+    private int to;
+
+    public Assignment(string range)
+    {
+        from = int.Parse(range[..range.IndexOf("-")]);
+        to = int.Parse(range[(range.IndexOf("-") + 1)..]);
+    }
+
+    public bool IsContained(Assignment other)
+    {
+        return from >= other.from && to <= other.to
+                || from <= other.from && to >= other.to;
+    }
+}
