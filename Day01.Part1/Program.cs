99f254419c9ee02d3418adf9c02f73bb51f743bf
Phomor <42185162+Phomor@users.noreply.github.com>
2022-12-04 13:32:36 +0100

    Day 01 Part 1

---
+++ Day01.Part1/Program.cs
@@ -0,0 +1,29 @@
+﻿var max = 0;
+
+try
+{
+    using var sr = new StreamReader("F:\\Documents\\AdventOfCode\\Day01.Part1\\input");
+    string? line;
+    var current = 0;
+    while ((line = sr.ReadLine()) != null)
+    {
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            max = Math.Max(max, current);
+            current = 0;
+        }
+        else
+        {
+            if (int.TryParse(line, out var num))
+            {
+                current += num;
+            }
+        }
+    }
+    Console.WriteLine(max);
+}
+catch (IOException ex)
+{
+    Console.WriteLine("File coult not be read");
+    Console.WriteLine(ex.ToString());
+}
