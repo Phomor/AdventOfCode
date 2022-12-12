ed4b59474eb074378f0bb529ef899f8e3a5e8df2
Phomor <42185162+Phomor@users.noreply.github.com>
2022-12-12 22:30:07 +0100

    Day 12

---
+++ Day12.Part1/Program.cs
@@ -0,0 +1,98 @@
+﻿var lines = File.ReadAllLines("input");
+Tile end = null!;
+var tiles = new Tile[lines.Length][];
+var startTiles = new List<Tile>();
+for (var y = 0; y < lines.Length; y++)
+{
+    tiles[y] = new Tile[lines[y].Length];
+    for (var x = 0; x < lines[y].Length; x++)
+    {
+        var c = lines[y][x];
+        tiles[y][x] = new Tile();
+        if (c == 'S')
+        {
+            c = 'a';
+        }
+        if (c == 'E')
+        {
+            end = tiles[y][x];
+            c = 'z';
+        }
+        if (c == 'a')
+        {
+            startTiles.Add(tiles[y][x]);
+        }
+        tiles[y][x].Height = c;
+        tiles[y][x].Coordinates = (y, x);
+    }
+}
+foreach (var tileList in tiles)
+{
+    foreach (var tile in tileList)
+    {
+         tile.SetNeighbours(tiles);
+    }
+}
+
+var minSteps = int.MaxValue;
+foreach (var start in startTiles)
+{
+    var queue = new Queue<Tile>();
+    queue.Enqueue(start);
+    start.Done = true;
+    start.ShortestDistance = 0;
+    while (queue.Count > 0)
+    {
+        var current = queue.Dequeue();
+        current.Done = true;
+        current.Neighbours.ForEach(p =>
+        {
+            if (!p.Done)
+            {
+                if (p.ShortestDistance > current.ShortestDistance + 1)
+                {
+                    p.ShortestDistance = current.ShortestDistance + 1;
+                }
+
+                p.Done = true;
+                queue.Enqueue(p);
+            }
+        });
+    }
+
+    if (end.ShortestDistance < minSteps)
+    {
+        minSteps = end.ShortestDistance;
+    }
+}
+Console.WriteLine(end.ShortestDistance);
+
+public class Tile
+{
+    public int Height;
+    public (int, int) Coordinates;
+    public bool Done;
+    public int ShortestDistance = int.MaxValue;
+    public List<Tile> Neighbours = new(4);
+    
+    public void SetNeighbours(Tile[][] tiles)
+    {
+        if (Coordinates.Item2 != 0)
+        {
+            Neighbours.Add(tiles[Coordinates.Item1][Coordinates.Item2 - 1]);
+        }
+        if (Coordinates.Item2 != tiles[Coordinates.Item1].Length - 1)
+        {
+            Neighbours.Add(tiles[Coordinates.Item1][Coordinates.Item2 + 1]);
+        }
+        if (Coordinates.Item1 != 0)
+        {
+            Neighbours.Add(tiles[Coordinates.Item1 - 1][Coordinates.Item2]);
+        }
+        if (Coordinates.Item1 != tiles.Length - 1)
+        {
+            Neighbours.Add(tiles[Coordinates.Item1 + 1][Coordinates.Item2]);
+        }
+        Neighbours = Neighbours.Where(p => p.Height <= Height + 1).ToList();
+    }
+}
