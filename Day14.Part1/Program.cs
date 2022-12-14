f45de48c36a7c42dc11ea998fe55a08cf32ed94f
Phomor <42185162+Phomor@users.noreply.github.com>
2022-12-14 19:26:21 +0100

    Day 14

---
+++ Day14.Part1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+
+var watch = new Stopwatch();
+watch.Start();
+
+using var sr = new StreamReader("input");
+string? line;
+var score = 0;
+var coords = new HashSet<(int x, int y)>();
+while ((line = sr.ReadLine()) != null)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        continue;
+    }
+    var lines = line.Split("->");
+    var first = lines[0].Trim().Split(",");
+    var (curX, curY) = (int.Parse(first[0]), int.Parse(first[1]));
+    coords.Add((curX, curY));
+    foreach (var path in lines.Skip(1))
+    {
+        var next = path.Trim().Split(",");
+        var (nextX, nextY) = (int.Parse(next[0]), int.Parse(next[1]));
+        while (curX != nextX || curY != nextY)
+        {
+            if (curX < nextX)
+            {
+                coords.Add((++curX, curY));
+            }
+            if (curX > nextX)
+            {
+                coords.Add((--curX, curY));
+            }
+            if (curY < nextY)
+            {
+                coords.Add((curX, ++curY));
+            }
+            if (curY > nextY)
+            {
+                coords.Add((curX, --curY));
+            }
+        }
+    }
+}
+
+var (x, y) = (500, 0);
+while (true)
+{
+    // no solid block below this one, falls into the abyss
+    if (!coords.Any(p => p.x == x && p.y > y))
+    {
+        break;
+    }
+    // try going straight down
+    if (!coords.Contains((x, y + 1)))
+    {
+        y++;
+    }
+    // straight down blocked, try going down and left
+    else if (!coords.Contains((x - 1, y + 1)))
+    {
+        x--;
+        y++;
+    }
+    // down and left blocked, try going down and right
+    else if (!coords.Contains((x + 1, y + 1)))
+    {
+        x++;
+        y++;
+    }
+    // down and right blocked, stay
+    else
+    {
+        score++;
+        coords.Add((x, y));
+        x = 500;
+        y = 0;
+    }
+}
+
+watch.Stop();
+Console.WriteLine($"Solution: {score}, time: {watch.Elapsed.TotalSeconds:N6}s");
--- Template/Program.cs
+++ Template/Program.cs
@@ -1,9 +1,14 @@
-﻿var score = 0;
+﻿using System.Diagnostics;
+
+var watch = new Stopwatch();
+watch.Start();
 
 using var sr = new StreamReader("input");
 string? line;
+var score = 0;
 while ((line = sr.ReadLine()) != null)
 {
 	
 }
-Console.WriteLine(score);
+watch.Stop();
+Console.WriteLine($"Solution: {score}, time: {watch.Elapsed.TotalSeconds:N6}s");
